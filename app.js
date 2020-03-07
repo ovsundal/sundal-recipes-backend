3892fc93f340b420c296b77809fff9b547746898
osund <osundal83@gmail.com>
2020-03-07 23:10:25 +0100

    add error handling

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
-import * as express from "express"
+const express = require("express");
+const bodyParser = require("body-parser");
+const usersRoutes = require("./routes/users-routes");
+const HttpError = require("./models/http-error");
 
 const app = express();
 
+app.use(bodyParser.json());
 
+app.use("/api/users/", usersRoutes);
+
+app.use((req, res, next) => {
+  throw new HttpError("Route not found", 404);
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.code || 500);
+  res.json({ message: err.message || "An unknown error occurred!" });
+});
+
+app.listen(5000);
