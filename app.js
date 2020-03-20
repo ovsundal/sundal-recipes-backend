b9641b5f09682d169cb2459e05dbfac2d6be2d04
osund <osundal83@gmail.com>
2020-03-20 20:23:13 +0100

    add headers

--- app.js
+++ app.js
@@ -9,6 +9,17 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.set("Access-Control-Allow-Origin", "*");
+  res.set(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.set("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+
+  next();
+});
+
 app.use("/api/users/", usersRoutes);
 app.use("/api/recipes/", recipesRoutes);
 
