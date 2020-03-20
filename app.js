f092f0640442e797fe89b6876be4b2b2644bc4db
osund <osundal83@gmail.com>
2020-03-20 23:40:24 +0100

    add headers

--- app.js
+++ app.js
@@ -10,12 +10,12 @@ const app = express();
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-  res.set("Access-Control-Allow-Origin", "*");
-  res.set(
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
-  res.set("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
 
   next();
 });
