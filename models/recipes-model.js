48d0d2e609a8f50bd43edc7cf1e6906c85cb8450
osund <osundal83@gmail.com>
2020-04-06 22:58:39 +0200

    add unique flag to each recipe

--- models/recipes-model.js
+++ models/recipes-model.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  recipe: { type: String, required: true }
+  recipe: { type: String, required: true, unique: true }
 });
 
 module.exports = mongoose.model("Recipe", recipeSchema);
