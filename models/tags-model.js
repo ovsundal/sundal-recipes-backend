31adc82f17b6ab386816ab9b0ae4da5466972fcb
osund <osundal83@gmail.com>
2020-04-28 22:24:48 +0200

    rename model property

--- models/tags-model.js
+++ models/tags-model.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const tagsSchema = new Schema({
-  tags: { type: String }
+  name: { type: String }
 });
 
 module.exports = mongoose.model("Tags", tagsSchema);
