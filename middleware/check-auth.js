f59aa4b71f53606c2efd106a3ddbeb48a2cb4d27
osund <osundal83@gmail.com>
2020-08-14 22:35:13 +0200

    add escape for OPTIONS methods

--- middleware/check-auth.js
+++ middleware/check-auth.js
@@ -3,6 +3,10 @@ const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
   try {
+    if (req.method === "OPTIONS") {
+      return next();
+    }
+
     const token = req.headers.authorization.split(" ")[1]; // Authorization: 'Bearer TOKEN'
 
     if (!token) {
@@ -10,7 +14,6 @@ module.exports = (req, res, next) => {
     }
 
     const decodedToken = jwt.verify(token, process.env.JWT_KEY);
-    console.log(decodedToken);
 
     req.userData = { userId: decodedToken.id };
 
