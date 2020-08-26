0042c989fd856b6dd7ed3cce0cc90394ac8883b2
osund <osundal83@gmail.com>
2020-08-26 19:46:59 +0200

    add validation check when adding tags

--- routes/tags-routes.js
+++ routes/tags-routes.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const tagsController = require("../controllers/tags-controller");
 const { check } = require("express-validator");
+const checkAuth = require("../middleware/check-auth");
 
 const router = express.Router();
 
 router.get("/getTags", tagsController.getTags);
 
 // routes below this requires token verification
-// router.use(checkAuth);
+router.use(checkAuth);
 
 router.post("/addTags", [check("name").notEmpty()], tagsController.addTags);
 
